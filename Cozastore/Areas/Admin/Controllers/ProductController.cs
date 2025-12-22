ed7640cf32e1a053bbbb333cd749003a16212e4e
ferhadguluzade1 <farhadfg-bpa202@code.edu.az>
2025-12-22 20:05:26 +0400

    new lesson folders added

---
+++ Cozastore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cozastore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
