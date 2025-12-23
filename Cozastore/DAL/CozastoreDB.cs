df5c858cfabeafaf07bd11232fc9483da5bf4c23
ferhadguluzade1 <farhadfg-bpa202@code.edu.az>
2025-12-23 19:36:01 +0400

    CozastoreDB was edited

--- Cozastore/DAL/CozastoreDB.cs
+++ Cozastore/DAL/CozastoreDB.cs
@@ -5,11 +5,14 @@ namespace Cozastore.DAL
 {
     public class CozastoreDB : DbContext
     {
-        public CozastoreDB(DbContextOptions options) : base(options)
-        {}
-        
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Image> Images { get; set; }
         public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        public CozastoreDB(DbContextOptions options) : base(options)
+        { }
     }
 }
