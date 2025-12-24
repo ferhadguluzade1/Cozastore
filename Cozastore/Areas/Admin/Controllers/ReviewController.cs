cfad005ee38d7cac16a485bdf70de04836870231
ferhadguluzade <farhadmedia20@gmail.com>
2025-12-24 11:05:35 +0400

    minor changes have been made.

---
+++ Cozastore/Areas/Admin/Controllers/ReviewController.cs
@@ -0,0 +1,113 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cozastore.ViewModels.ReviewViewModel;
+using Cozastore.DAL;
+using Cozastore.Models;
+
+namespace Cozastore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ReviewController : Controller
+    {
+        CozastoreDB _db;
+        public ReviewController(Cozastore db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var review = _db.Reviews
+            .ToList();
+            return View(review);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Products = _db.Products.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateReviewViewModel reviewViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Products = _db.Products.ToList();
+                return View(reviewViewModel);
+            }
+
+            Review review = new Review
+            {
+                Comment = reviewViewModel.Comment,
+                ProductsId = reviewViewModel.ProductId
+            };
+
+            await _db.Reviews.AddAsync(review);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return View("Error");
+            var review = await _db.Reviews.FirstOrDefault(r => r.Id == id);
+            if (review == null) return View("Error");
+            review.IsDeleted = true;
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Restore(int? id)
+        {
+            if (id == null) return View("Error");
+            var review = await _db.Reviews.FirstOrDefaultAsync(r => r.Id = id);
+            if (review == null) review View("Error);
+            review.IsDeleted = false;
+            await _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null) return View("Error");
+            var review = await _db.Reviews.FirstOrDefaultAsync(r => r.Id == id);
+            if (review == null) return View("Error");
+            ViewBag.Products = _db.Products.ToList();
+            UpdateReviewViewModel reviewViewModel = new UpdateReviewViewModel
+            {
+                Id = review.Id,
+                Comment = review.Comment,
+                ProductId = review.ProductId
+            };
+            return View(reviewViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateReviewViewModel reviewViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Products = _db.Products.ToList();
+                return View(reviewViewModel);
+            }
+
+            var review = await _db.Reviews.FirstOrDefaultAsync(r => r.Id == reviewViewModel.Id);
+            if (review == null) return View("Error");
+            review.Comment = reviewViewModel.Comment;
+            review.ProductId = reviewViewModel.ProductId;
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        
+        }
+
+
+       
+
+
+    }
+
+
+
+}
--- Cozastore/Areas/Admin/Controllers/SliderController.cs
+++ Cozastore/Areas/Admin/Controllers/SliderController.cs
@@ -20,7 +20,8 @@ namespace Cozastore.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            List<Slider> sliders = _db.Sliders.ToList();
+            List<Slider> sliders = _db.Sliders
+                .ToList();
             return View(sliders);
         }
 
@@ -29,43 +30,56 @@ namespace Cozastore.Areas.Admin.Controllers
             return View();
         }
 
+        [HttpPost]
         public IActionResult Create(Slider slider)
         {
-            //if (!slider.ImageFile.ContentType.Contains("image/"))
-            //{
-            //    ModelState.AddModelError("ImageFile", "File type must be image");
-            //    return View();
-            //}
-            //if(slider.ImageFile.Length > 2 * 1024 * 1024)
-            //{
-            //    ModelState.AddModelError("ImageFile", "Image size must be max 2MB");
-            //    return View();
-            //}
-            //var fileName = slider.ImageFile.SaveImage(_env, "Uploads/Slider");
-            //slider.Image = fileName;
+            if (!slider.ImageFile.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("ImageFile", "File type must be image");
+                return View();
+            }
+            if(slider.ImageFile.Length > 2 * 1024 * 1024)
+            {
+                ModelState.AddModelError("ImageFile", "Image size must be max 2MB");
+                return View();
+            }
+            var fileName = slider.ImageFile.SaveImage(_env, "Uploads/Slider");
+            slider.ImageUrl = fileName;
             if (!ModelState.IsValid) return View();
-            if (slider == null) return NotFound();
+            if (slider == null) return View("Error");
             _db.Sliders.Add(slider);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
 
+        [HttpPost]
         public IActionResult Delete(int? id)
         {
-            if (id == null) return NotFound();
-            Slider slider = _db.Sliders.FirstOrDefault(item => item.Id == id);
-            if (slider == null) return NotFound();
-            //slider.ImageUrl?.Delete(_env, "Uploads/Slider");
-            _db.Sliders.RemoveRange();
+            if (id == null) return View("Error");
+            Slider? slider = _db.Sliders.FirstOrDefault(item => item.Id == id);
+            if (slider == null) return View("Error");
+            slider.IsDeleted = true;
+            _db.SaveChanges();
             return RedirectToAction("Index");
 
         }
 
-        public IActionResult Update(int id)
+        [HttpPost]
+        public IActionResult Restore(int? id)
         {
-            //if (id == null) return NotFound();
-            Slider slider = _db.Sliders.FirstOrDefault(item => item.Id == id);
-            if (slider == null) return NotFound();
+            if (id == null) return View("Error");
+            Slider? slider = _db.Sliders.FirstOrDefault(item => item.Id == id);
+            if (slider == null) return View("Error");
+            slider.IsDeleted = false;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int? id)
+        {
+            if (id == null) return View("Error");
+            Slider? slider = _db.Sliders.FirstOrDefault(item => item.Id == id);
+            if (slider == null) return View("Error");
             return View(slider);
         }
 
@@ -73,9 +87,9 @@ namespace Cozastore.Areas.Admin.Controllers
         public IActionResult Update(Slider newSlider)
         {
             if (!ModelState.IsValid) return View();
-            if (newSlider == null) return NotFound();
-            Slider oldSlider = _db.Sliders.FirstOrDefault(item => item.Id == newSlider.Id);
-            if (oldSlider == null) return NotFound();
+            if (newSlider == null) return View("Error");
+            Slider? oldSlider = _db.Sliders.FirstOrDefault(item => item.Id == newSlider.Id);
+            if (oldSlider == null) return View("Error");
 
             oldSlider.Title = newSlider.Title;
             oldSlider.Description = newSlider.Description;
